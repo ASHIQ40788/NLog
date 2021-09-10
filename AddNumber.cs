b7eaa72d6c63733330f71662c654e394b070e77d
ShaikAshiq <ashiqshaiksvali8@gmail.comgit config --global user.email ashiqshaiksvali8@gmail.com>
2021-09-10 14:51:54 +0530

    [ASHIQ] added : NLog PROGRAM FILES

---
+++ AddNumber.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLogDemo058
+{
+    class AddNumber
+    {
+        private readonly NLog nlog = new NLog();
+        public int Sum(int FirstNumber,int secondNumber)
+        {
+            if (FirstNumber == 0 || secondNumber == 0)
+            {
+                nlog.LogDebug("Debug Unsuccessfull:Sum()");
+                nlog.LogError("Null value");
+                nlog.LogWarn("FirstNumber or secondNumber should not equal to 0");
+                return 0;
+            }
+            else
+            {
+                int result = secondNumber + FirstNumber;
+                nlog.LogDebug("Debug successfull:Sum()");
+                nlog.LogInfo("sum method passed,Result:" + result);
+                return result;
+            }
+
+            
+        }
+
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLogDemo058
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number:");
+            int FirstNumb = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            int secNumb = Convert.ToInt32(Console.ReadLine());
+            AddNumber addNumber = new AddNumber();
+            Console.WriteLine("Result:" + addNumber.Sum(FirstNumb, secNumb));
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+}
